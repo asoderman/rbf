28eadcbea8ee7b40cc16f61620045b68443d44e7
Alex Soderman <alex@Alexs-MacBook-Air.local>
2021-10-31 13:17:12 -0400

    Clean up nasm warnings

--- src/compiler.rs
+++ src/compiler.rs
@@ -2,14 +2,14 @@ use std::collections::HashMap;
 
 lazy_static!{
     static ref SYMBOL_MAP: HashMap<char, &'static str> = hashmap! {
-            '>' => "    inc     r8\n",
-            '<' => "    dec     r8\n",
-            '+' => "    inc     byte [r8]\n",
-            '-' => "    dec     byte [r8]\n",
-            '.' => "    mov     r10b, byte [r8]\n    mov    byte [r9], r10b\n    inc    r9\n",
+            '>' => "    inc    r8\n",
+            '<' => "    dec    r8\n",
+            '+' => "    inc    byte [r8]\n",
+            '-' => "    dec    byte [r8]\n",
+            '.' => "    mov    r10b, byte [r8]\n    mov    byte [r9], r10b\n    inc    r9\n",
             ',' => "\n",
             '[' => "\n.loop{}:\n",
-            ']' => "    cmp     byte [r8], 0\n    jnz     .loop{}\n"
+            ']' => "    cmp     byte [r8], 0\n    jnz    .loop{}\n"
     };
 }
 
@@ -48,8 +48,8 @@ impl Compilation {
         output.push_str("section   .text\n");
         output.push_str("_main:  \n");
         output.push_str("    sub    rsp, 8 ; Align the stack pointer \n");
-        output.push_str("    mov    r8, cell ; r8 will be out data pointer\n");
-        output.push_str("    mov    r9, output ; r9 will be out output pointer\n");
+        output.push_str("    mov    r8, cell ; r8 will be our data pointer\n");
+        output.push_str("    mov    r9, output ; r9 will be our output pointer\n");
 
         let mut loop_counter = 0;
         let mut loops_open = Vec::new();
@@ -70,10 +70,10 @@ impl Compilation {
             }
         }
 
-        output.push_str("mov rax, 0\n");
-        output.push_str("mov rdi, output\n");
-        output.push_str("call _printf\n");
-        output.push_str("add rsp, 8 ; Undo stack alignment\n");
+        output.push_str("    mov rax, 0\n");
+        output.push_str("    mov rdi, output\n");
+        output.push_str("    call _printf\n");
+        output.push_str("    add rsp, 8 ; Undo stack alignment\n");
         output.push_str("ret\n");
         output.push_str("    mov    rax, 0x2000001 ; Exit\n");
         output.push_str("    mov    rdi, 0\n");
@@ -82,10 +82,8 @@ impl Compilation {
         // bss
         output.push_str("section   .bss\n");
         output.push('\n');
-        output.push_str(r#"format: db "%c""#);
-        output.push('\n');
-        output.push_str(&format!("output: times {} db 0\n", self.output_calls()));
-        output.push_str(&format!("cell:   times {} db 0\n", self.cells()));
+        output.push_str(&format!("output: resb {} \n", self.output_calls()));
+        output.push_str(&format!("cell:   resb {} \n", self.cells()));
 
         output
     }
